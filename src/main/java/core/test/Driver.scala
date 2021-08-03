1804598853a0a13e963652bcc4d5b9b9ab7ce616
DrDavidS <312648004@qq.com>
2021-08-04 00:59:43 +0800

    添加注释说明

--- src/main/java/core/test/Driver.scala
+++ src/main/java/core/test/Driver.scala
@@ -7,22 +7,22 @@ object Driver {
   def main(args: Array[String]): Unit = {
     // 连接服务器
     // 两个计算节点
-    val client1 = new Socket("localhost", 9999)
-    val client2 = new Socket("localhost", 8888)
+    val client1 = new Socket("localhost", 9999) // 客户端1请求
+    val client2 = new Socket("localhost", 8888) // 客户端2请求
 
     val task = new Task()
 
     // 发送给Executor1
     val out1: OutputStream = client1.getOutputStream
-    val objOut1 = new ObjectOutputStream(out1)
+    val objOut1 = new ObjectOutputStream(out1) //将指定的对象写入 ObjectOutputStream
     // 拆分，subTask拿前半部
     val subTask = new SubTask()
     subTask.logic = task.logic
     subTask.datum = task.datum.take(2)
 
     objOut1.writeObject(subTask)
-    objOut1.flush()
-    objOut1.close()
+    objOut1.flush() // 清空缓冲区
+    objOut1.close() // 关闭IO流
 
     client1.close()
     println("客户端1数据发送完毕")
@@ -30,15 +30,15 @@ object Driver {
 
     // 发送给Executor2
     val out2: OutputStream = client2.getOutputStream
-    val objOut2 = new ObjectOutputStream(out2)
+    val objOut2 = new ObjectOutputStream(out2) //将指定的对象写入 ObjectOutputStream
     // 拆分，subTask2拿后半部
     val subTask2 = new SubTask()
     subTask2.logic = task.logic
     subTask2.datum = task.datum.takeRight(2)
 
     objOut2.writeObject(subTask2)
-    objOut2.flush()
-    objOut2.close()
+    objOut2.flush() // 清空缓冲区
+    objOut2.close() // 关闭IO流
 
     client2.close()
     println("客户端2数据发送完毕")
--- src/main/java/core/test/Executor.scala
+++ src/main/java/core/test/Executor.scala
@@ -3,18 +3,26 @@ package core.test
 import java.io.{InputStream, ObjectInputStream}
 import java.net.{ServerSocket, Socket}
 
+/**
+ * 执行节点1
+ * 运行时，先启动SocketServer，再启动SocketClient。
+ *
+ * 在先启动SocketServer时，代码执行到socket.receive(packet)时
+ * 会一直阻塞在这里，直到启动SocketClient后，
+ * SocketServer会继续执行，并将收到SocketClient的信息打印出来
+ */
 
 // 先执行 Executor，再启动Driver
 object Executor {
   def main(args: Array[String]): Unit = {
     // 启动服务器接收数据
-    val server = new ServerSocket(9999)
+    val server = new ServerSocket(9999) // 服务端监听端口9999，现在处于等待连接的状态
     println("服务器[9999]启动，等待接收数据")
 
     // 等待客户端的连接
-    val client: Socket = server.accept()
-    val in: InputStream = client.getInputStream
-    val objIn = new ObjectInputStream(in)
+    val client: Socket = server.accept() // 使用accept监听来自客户端的连接
+    val in: InputStream = client.getInputStream // 获取字节流
+    val objIn = new ObjectInputStream(in) // 从获取的字节流中创建一个反序列化流
     val task: SubTask = objIn.readObject().asInstanceOf[SubTask]
     val ints: List[Int] = task.compute()  // 计算
 
