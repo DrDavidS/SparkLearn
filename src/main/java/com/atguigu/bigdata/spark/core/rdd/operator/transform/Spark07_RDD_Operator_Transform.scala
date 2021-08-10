316a681982d3b768fdafc7eb0e4a3589469c7908
银汉 <daichuan.ydc@alibaba-inc.com>
2021-08-10 14:46:49 +0800

    修正

--- src/main/java/com/atguigu/bigdata/spark/core/rdd/operator/transform/Spark07_RDD_Operator_Transform.scala
+++ src/main/java/com/atguigu/bigdata/spark/core/rdd/operator/transform/Spark07_RDD_Operator_Transform.scala
@@ -3,10 +3,14 @@ package com.atguigu.bigdata.spark.core.rdd.operator.transform
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
+import java.text.SimpleDateFormat
+import java.util.Date
+
 /**
- * P56 https://www.bilibili.com/video/BV11A411L7CK?p=56
- * sample 的用法
+ * P55 https://www.bilibili.com/video/BV11A411L7CK?p=55
+ * filter 的用法
  *
+ *  数据筛选过滤后没分区
  */
 
 object Spark07_RDD_Operator_Transform {
@@ -16,17 +20,12 @@ object Spark07_RDD_Operator_Transform {
     val sc = new SparkContext(sparkConf)
 
 
-    // TODO 算子 - sample
-    val rdd: RDD[Int] = sc.makeRDD(List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+    // TODO 算子 - filter
+    val rdd: RDD[Int] = sc.makeRDD(List(1,2,3,4))
 
+    val filterRDD: RDD[Int] = rdd.filter(num => num % 2 != 0)
 
-    // sample 需要三个参数
-    rdd.sample(
-      withReplacement = false, // 1. 是否要放回
-      0.4, // 2. 如果是抽取不放回：数据源中每条数据被抽取的概率。
-                    // 如果是抽取放回，表示数据源中每条数据被抽取的可能次数
-      888 // 3. 随机种子，如果不传就是系统时间
-    ).collect().foreach(println)
+    filterRDD.collect().foreach(println)
 
     sc.stop()
   }
