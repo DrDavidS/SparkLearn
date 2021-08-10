8778357727e90cd9a733b0eb3ea393f27698d4b6
银汉 <daichuan.ydc@alibaba-inc.com>
2021-08-10 19:04:59 +0800

    groupByKey

--- src/main/java/com/atguigu/bigdata/spark/core/rdd/operator/transform/Spark16_RDD_Operator_Transform.scala
+++ src/main/java/com/atguigu/bigdata/spark/core/rdd/operator/transform/Spark16_RDD_Operator_Transform.scala
@@ -7,9 +7,16 @@ import org.apache.spark.{SparkConf, SparkContext}
  * P66 https://www.bilibili.com/video/BV11A411L7CK?p=66
  * KV类型 groupByKey
  *
- * 和 groupBy的区别？
- *
  * groupByKey 会导致数据打乱重组，存在shuffle操作
+ * 在Spark中，shuffle操作必须落盘，不能再内存中等待，否则会内存溢出
+ * shuffle操作的性能很低，因为存在磁盘IO
+ *
+ * 再优化：在每个分区提前聚合一部分，再落盘。这样IO少了，性能提升了
+ *
+ * reduceByKey 支持分区内预先聚合，有效减少shuffle落盘的数据量，性能比较高
+ *
+ * 所以分组聚合的情况下推荐reduceByKey，只分组不聚合，就用groupByKey
+ *
  */
 
 object Spark16_RDD_Operator_Transform {
