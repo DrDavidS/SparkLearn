30d6ff9ca744a138238762c4ae1b186b844fac2a
银汉 <daichuan.ydc@alibaba-inc.com>
2021-09-02 11:49:37 +0800

    Pregel API 中文注释

--- src/main/GraphX/G06_Pregel.scala
+++ src/main/GraphX/G06_Pregel.scala
@@ -1,11 +1,13 @@
 import org.apache.spark.graphx.util.GraphGenerators
-import org.apache.spark.graphx.{EdgeTriplet, Graph, VertexId}
+import org.apache.spark.graphx.{Edge, EdgeTriplet, Graph, VertexId}
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
  * Pregel API
  *
  * 使用 Pregel 算子来表达计算，例如单源最短路径。
+ * 注意，这里最关键的就是 pregel 函数。
+ * 计算原理参考 https://blog.csdn.net/qq_38265137/article/details/80547763
  */
 
 object G06_Pregel {
@@ -13,20 +15,23 @@ object G06_Pregel {
     val sparkConf: SparkConf = new SparkConf().setMaster("local").setAppName("GraphX Ops")
     val sc = new SparkContext(sparkConf)
 
-    // A graph with edge attributes containing distances
+    /* 包含距离的边数据的图
+    用 GraphGenerators 的 logNormalGraph 方法生成了一个正态分布的，具有100个顶点的图
+    其中对边的距离属性转换为double类型
+     */
     val graph: Graph[Long, Double] =
-      GraphGenerators.logNormalGraph(sc, numVertices = 100).mapEdges(e => e.attr.toDouble)
+      GraphGenerators.logNormalGraph(sc, numVertices = 100).mapEdges((e: Edge[Int]) => e.attr.toDouble)
 
-    val sourceId: VertexId = 42 // The ultimate source
+    val sourceId: VertexId = 42 // 默认点ID，神奇的42
 
-    // Initialize the graph such that all vertices except the root have distance infinity.
-    val initialGraph: Graph[Double, Double] = graph.mapVertices((id, _) =>
+    // 初始化图，使得除根之外的所有顶点都具有无穷远的距离。
+    val initialGraph: Graph[Double, Double] = graph.mapVertices((id: VertexId, _: VertexId) =>
       if (id == sourceId) 0.0 else Double.PositiveInfinity)
 
     val sssp: Graph[Double, Double] = initialGraph.pregel(Double.PositiveInfinity)(
       (id: VertexId, dist: Double, newDist: Double) =>
-        math.min(dist, newDist), // Vertex Program
-      (triplet: EdgeTriplet[Double, Double]) => { // Send Message
+        math.min(dist, newDist), // 顶点程序
+      (triplet: EdgeTriplet[Double, Double]) => { // 发送消息
         if (triplet.srcAttr + triplet.attr < triplet.dstAttr) {
           Iterator((triplet.dstId, triplet.srcAttr + triplet.attr))
         } else {
