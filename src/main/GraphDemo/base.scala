ce9940e456ec2a839eea94e007dd69e9c9f2405c
DrDavidS <daichuan.ydc@antgroup.com>
2021-09-18 11:41:49 +0800

    修正层级默认值为1

--- src/main/GraphDemo/base.scala
+++ src/main/GraphDemo/base.scala
@@ -10,9 +10,9 @@ case class baseProperties(
 
 case class investmentInfo(
                            investedComName: String = "default_name", // 被投资企业的名称，和Key对应
-                           proportionOfI: String = "0.00", // 投资占比
+                           proportionOfInvestment: String = "0.00", // 投资占比
                            oneInvestment: BigDecimal = 0, // 一个投资金额
                            totalInvestment: BigDecimal = 0, // 被投资对象的总注册资本（总投资金额）
                            upperStream: VertexId = 99998L, // 此对象的上游对象（投资方）
-                           level: String = "-1" // 距离当前节点的层级
+                           level: Int = 1 // 距离当前节点的层级
                          )
