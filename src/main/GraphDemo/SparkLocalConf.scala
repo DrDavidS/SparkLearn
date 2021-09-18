8b57ec8b182d4873d50199543f16d6483754e8dd
DrDavidS <daichuan.ydc@antgroup.com>
2021-09-18 11:04:53 +0800

    统一管理本地SparkContext

---
+++ src/main/GraphDemo/SparkLocalConf.scala
@@ -0,0 +1,7 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+case class SparkLocalConf(
+                           sc: SparkContext = new SparkContext(
+                             new SparkConf().setMaster("local").setAppName("GraphX Ops")
+                           )
+                         )
