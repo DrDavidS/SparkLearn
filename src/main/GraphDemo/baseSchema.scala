24910eb986d1fafac9f86b2a29f5cad27cb41b19
DrDavidS <daichuan.ydc@antgroup.com>
2021-10-11 18:50:36 +0800

    新增简化类型

--- src/main/GraphDemo/baseSchema.scala
+++ src/main/GraphDemo/baseSchema.scala
@@ -12,5 +12,18 @@ case class investmentInfo(
                            registeredCapital: BigDecimal = 0, // 被投资对象的总注册资本
                            upperStreamId: VertexId = 99998L, // 此对象的上游对象（投资方）
                            level: Int = 1, // 距离当前节点的层级
-                           addSign:Boolean = false, // 属于Src节点信息，避免重复相加
+                           addSign: Boolean = false, // 属于Src节点信息，避免重复相加
                          )
+
+case class simpleProperties(
+                             name: String, // 名称
+                             oneStepInvInfo: Map[VertexId, simpleInvestmentInfo] // N级投资对象的持股信息
+                           )
+
+case class simpleInvestmentInfo(
+                                 investedComName: String = "default_name", // 被投资企业的名称，和Key对应
+                                 proportionOfInvestment: String = "0.0", // 投资占比
+                                 upperStreamId: VertexId = 99998L, // 此对象的上游对象（投资方）
+                                 level: Int = 1, // 距离当前节点的层级
+                               )
+
