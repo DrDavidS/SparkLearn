4c262c7fc376185ecbb1c0bd5ecf96bce1b668fa
银汉 <daichuan.ydc@alibaba-inc.com>
2021-10-18 17:42:05 +0800

    更加精简属性

--- src/main/GraphDemo/baseSchema.scala
+++ src/main/GraphDemo/baseSchema.scala
@@ -18,15 +18,14 @@ case class investmentInfo(
 case class simpleProperties(
                              name: String, // 名称
                              nStepInvInfo: Map[VertexId, simpleInvestmentInfo], // N级投资对象的持股信息
-                             initInvInfo: Map[VertexId, simpleInvestmentInfo], // 保存的是初始一步投资数据
-                             lastCycleInvInfo: Map[VertexId, simpleInvestmentInfo], // 成环当时的数据
                            )
 
 case class simpleInvestmentInfo(
                                  investedComName: String = "default_name", // 被投资企业的名称，和Key对应
-                                 proportionOfInvestment: String = "0.0", // 投资占比
-                                 upperStreamId: VertexId = 99998L, // 此对象的上游对象（投资方）
-                                 level: Int = 1, // 距离当前节点的层级
-                                 ifCycle: Boolean = false // 如果成环会变成true
+                                 proportionOfInvestment: BigDecimal = 0.000000, // 投资占比
                                )
 
+case class Attr(
+                 neigh: List[VertexId],
+                 values: Map[VertexId, Double]
+               )
