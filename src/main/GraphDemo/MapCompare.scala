3ae9bd0a663cbb693afda9784c15b771b9c0f488
银汉 <daichuan.ydc@alibaba-inc.com>
2021-10-25 20:43:00 +0800

    测试用

--- src/main/GraphDemo/MapCompare.scala
+++ src/main/GraphDemo/MapCompare.scala
@@ -3,5 +3,6 @@ object MapCompare {
     val immutableMap = Map("Jim" -> 22, "yxj" -> 32)
     val stringToInt: Map[String, Int] = immutableMap.map((row: (String, Int)) => (row._1, row._2))
     println(stringToInt)
+    println(immutableMap)
   }
 }
