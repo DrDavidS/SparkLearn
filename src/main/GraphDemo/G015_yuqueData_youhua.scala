a1a4f03ade06de53d90badd5366eab23160f888c
银汉 <daichuan.ydc@alibaba-inc.com>
2021-10-28 09:39:38 +0800

    中间步骤修正

--- src/main/GraphDemo/G015_yuqueData_youhua.scala
+++ src/main/GraphDemo/G015_yuqueData_youhua.scala
@@ -94,7 +94,7 @@ object G015_yuqueData_youhua {
    * @param stepGraph 每次迭代输入的图
    * @param initGraph 初始图，用于Join
    * @return
-  */
+   */
   def graphCalculate(stepGraph: Graph[Map[VertexId, Map[VertexId, Double]], Double],
                      initGraph: Graph[Map[VertexId, Map[VertexId, Double]], Double]
                     ): Graph[Map[VertexId, Map[VertexId, Double]], Double] = {
@@ -122,55 +122,56 @@ object G015_yuqueData_youhua {
         val idToDoubleSerialized: Map[VertexId, Double] = idToDouble.map(
           (row: (VertexId, Double)) => (row._1, row._2))
 
+        // TODO 本次发送的消息和上次发送的消息差异过小，可以不发送
         triplet.sendToSrc(Map(dstId -> idToDoubleSerialized))
       },
       // merge message
-      (m1, m2) => {
-        m1 ++ m2
-      }
-    )
-    // nGraph.collect.foreach(println)
-
-    // 将上面计算的结果Join到图里面
-    val loopGraph: Graph[Map[VertexId, Map[VertexId, Double]], Double] = initGraph.outerJoinVertices(msgVertexRDD)(
-      (vid: VertexId,
-       vdata: Map[VertexId, Map[VertexId, Double]], // 图中顶点原有数据
-       nvdata: Option[Map[VertexId, Map[VertexId, Double]]] // 要join的数据
-      ) => {
-        val ndata: Map[VertexId, Map[VertexId, Double]] = nvdata.getOrElse(Map.empty) // 要join的数据【去除空值】
-        val unionData: Map[VertexId, Map[VertexId, Double]] = vdata.map((row: (VertexId, Map[VertexId, Double])) => {
-          // 针对原来的图的顶点数据，做map操作
-
-          val statrUnionMap: Map[VertexId, Double] = row._2
-          val newValue: Map[VertexId, Double] = if (ndata.contains(row._1)) // 如果当前顶点ID在被join的ndata中（说明发送的顶点相同）
-          {
-            val vRatio: Map[VertexId, Double] = row._2 // vdata中的持股比例
-            val nRatio: Map[VertexId, Double] = ndata(row._1) // 同时在ndata中找到新计算的持股比例
-
-            // 增量的持股信息
-            // 1. 在nRatio中做一个过滤，筛选出新增的持股信息【不在vdata里面的】
-            // TODO 这里是不是能优化一下
-            val newKeyMaps: Map[VertexId, Double] = nRatio.filter((r: (VertexId, Double)) => {
-              !vRatio.contains(r._1)
-            })
-
-            // 2. 在vRatio中做一个过滤，筛选出存量的持股信息【已经存在于ndata里面的】
-            val reserveMaps: Map[VertexId, Double] = vRatio.filter((r: (VertexId, Double)) => {
-              nRatio.contains(r._1)
-            }).map((row: (VertexId, Double)) => (row._1, row._2 + nRatio(row._1)))
-
-            // 3. 存量的（差异的保留） 在vRatio中做一个过滤，筛选出存量的持股信息【不存在于ndata里面的】
-            val diffNN: Map[VertexId, Double] = vRatio.filter(r => {
-              !nRatio.contains(r._1)
-            })
-            val unionMap: Map[VertexId, Double] = diffNN ++ reserveMaps ++ newKeyMaps
-            unionMap
-          } else statrUnionMap // 节点对子公司的持股比例【原始】
-          (row._1, newValue)
-        })
-        unionData
-      })
-    // loopGraph.vertices.collect.foreach(println)
-    loopGraph // 返回的Graph
+      _ ++ _
   }
+
+  )
+  // nGraph.collect.foreach(println)
+
+  // 将上面计算的结果Join到图里面
+  val loopGraph: Graph[Map[VertexId, Map[VertexId, Double]], Double] = initGraph.outerJoinVertices(msgVertexRDD)(
+    (vid: VertexId,
+     vdata: Map[VertexId, Map[VertexId, Double]], // 图中顶点原有数据
+     nvdata: Option[Map[VertexId, Map[VertexId, Double]]] // 要join的数据
+    ) => {
+      val ndata: Map[VertexId, Map[VertexId, Double]] = nvdata.getOrElse(Map.empty) // 要join的数据【去除空值】
+      val unionData: Map[VertexId, Map[VertexId, Double]] = vdata.map((row: (VertexId, Map[VertexId, Double])) => {
+        // 针对原来的图的顶点数据，做map操作
+
+        val statrUnionMap: Map[VertexId, Double] = row._2
+        val newValue: Map[VertexId, Double] = if (ndata.contains(row._1)) // 如果当前顶点ID在被join的ndata中（说明发送的顶点相同）
+        {
+          val vRatio: Map[VertexId, Double] = row._2 // vdata中的持股比例
+          val nRatio: Map[VertexId, Double] = ndata(row._1) // 同时在ndata中找到新计算的持股比例
+
+          // 增量的持股信息
+          // 1. 在nRatio中做一个过滤，筛选出新增的持股信息【不在vdata里面的】
+          // TODO 这里是不是能优化一下
+          val newKeyMaps: Map[VertexId, Double] = nRatio.filter((r: (VertexId, Double)) => {
+            !vRatio.contains(r._1)
+          })
+
+          // 2. 在vRatio中做一个过滤，筛选出存量的持股信息【已经存在于ndata里面的】
+          val reserveMaps: Map[VertexId, Double] = vRatio.filter((r: (VertexId, Double)) => {
+            nRatio.contains(r._1)
+          }).map((row: (VertexId, Double)) => (row._1, row._2 + nRatio(row._1)))
+
+          // 3. 存量的（差异的保留） 在vRatio中做一个过滤，筛选出存量的持股信息【不存在于ndata里面的】
+          val diffNN: Map[VertexId, Double] = vRatio.filter(r => {
+            !nRatio.contains(r._1)
+          })
+          val unionMap: Map[VertexId, Double] = diffNN ++ reserveMaps ++ newKeyMaps
+          unionMap
+        } else statrUnionMap // 节点对子公司的持股比例【原始】
+        (row._1, newValue)
+      })
+      unionData
+    })
+  // loopGraph.vertices.collect.foreach(println)
+  loopGraph // 返回的Graph
+}
 }
